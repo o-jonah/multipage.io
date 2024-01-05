81f2cac06f0af40d3a3f8a10f4299bca5bdef9fc
Jonah Odongo <126646855+o-jonah@users.noreply.github.com>
2024-01-05 15:02:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// function startTypingAnimation() {
+  //const heading = document.querySelector('.typewriter h1');
+  //heading.style.animation = 'typing 6s steps(38), blink-caret 0.9s step-end infinite';
+//}
+
+//Trigger the animation when the page loads
+//window.onload = startTypingAnimation;
